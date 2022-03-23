608d2f6c1e7f3cf2e44f878defda3fd05385033b
gaojunonly1 <2596819858@qq.com>
2022-03-23 21:13:45 +0800

    测试

---
+++ Gqueue/Gqueue.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "Gqueue.h"
+using namespace std;
+int main() {
+	Gqueue<char> alb;
+	alb.Ginsert_head('a');
+	alb.Ginsert_head('l');
+	alb.Ginsert_head('b');
+	alb.Gshow();
+	return 0;
+}
